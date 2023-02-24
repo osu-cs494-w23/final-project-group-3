199a63d5e2eea3dcd53c8467686c5df08cc4df70
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-02-24 14:57:19 -0800

    add autocomplete to test page and improve aesthetics

--- src/components/RedfinTest.js
+++ src/components/RedfinTest.js
@@ -1,8 +1,8 @@
 import React from "react";
-import {useRedfinListingsApi} from "../hooks/useRedfinApi";
+import {useRedfinListingsApi, useRedfinAutoCompleteApi} from "../hooks/useRedfinApi";
 
 function RedfinTest(props) {
-  const [{ data, loading, isError }] = useRedfinListingsApi({
+  const [listings] = useRedfinListingsApi({
     region_id: '30749',
     region_type: '6',
     uipt: '1,2,3,4,7,8',
@@ -10,12 +10,36 @@ function RedfinTest(props) {
     sf: '1,2,3,5,6,7',
     num_homes: '10',
   }, false);
+
+  const preCodeStyle = {
+    textAlign: 'left',
+    padding: '2rem',
+    backgroundColor:'black',
+    fontSize: '12px',
+    fontFamily: 'sans-serif'
+  }
+
+  const [autoComplete] = useRedfinAutoCompleteApi('san francisco', false);
   return (
-    <div>
+    <div style={{overflowY: 'auto', height: '100%', width:'100%', flexWrap: 'wrap'}}>
       <p>Redfin Test</p>
-      <p>You know things are working if you see json all over the screen</p>
-      {loading ? ( <div>Loading...</div> ) : ( <div>{JSON.stringify(data)}</div> )}
-      {isError && <div>Something went wrong ...</div>}
+      <h1>AutoComplete endpoint</h1>
+      <div style={{wordWrap:'break-word'}}>
+        {autoComplete.isLoading && !autoComplete.isError ? ( <p>Loading...</p> ) : (
+            <pre style={preCodeStyle}>
+              {JSON.stringify(autoComplete.data,null,2.5)}
+            </pre>)}
+        {autoComplete.isError && <p>Something went wrong ...</p>}
+      </div>
+      <br></br>
+      <h1>Listings endpoint</h1>
+      <div style={{padding:'1rem', wordWrap:'break-word'}}>
+        {listings.isLoading && !autoComplete.isError ? (<p>Loading...</p>) : (
+            <pre style={preCodeStyle}>
+              {JSON.stringify(autoComplete.data,null,2.5)}
+            </pre>)}
+        {listings.isError && <p>Something went wrong ...</p>}
+      </div>
     </div>
   );
 }
