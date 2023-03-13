ff5f3fc70225ed95db6b5bee2b7c41f0a78d0d89
Seth Weiss <sweisss@gmail.com>
2023-03-13 16:30:53 -0700

    Add whitespace in History.js

--- src/components/History.js
+++ src/components/History.js
@@ -1,8 +1,10 @@
 import React from "react";
 import {useRedfinListingsApi, useRedfinAutoCompleteApi} from "../hooks/useRedfinApi";
+
 function History(props) {
     return (
         <h1>Testing. 1, 2, 3...</h1>
     )
 }
+
 export default History
