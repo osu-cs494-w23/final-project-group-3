71d30111485ea30ce2c8c63c7861fe0a6adc2f80
Seth Weiss <sweisss@gmail.com>
2023-03-13 20:28:21 -0700

    Move csv parsing logic to hook.

--- src/components/History.js
+++ src/components/History.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react"
 import Papa from 'papaparse'
+import { useState } from "react";
+
+const csvFilePath = require("../data/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv")
+
+function useCSV(csvFilePath) {
+    const [ prices, setPrices ] = useState([])
+    const [ isLoading, setIsLoading ] = useState(false)
+    useEffect(() => {
+        setIsLoading(true)
+        Papa.parse(csvFilePath, {
+            header: true,
+            download: true,
+            skipEmptyLines: true,
+            complete: function(results) {
+                console.log("in Papa.parse:", results)
+                // setCity(results)
+                setIsLoading(false)
+                setPrices(results)
+            }
+        })
+    }, [])
+    
+    return { prices, isLoading}
+}
 
 function History() {
-    const csvFilePath = require("../data/Metro_zhvi_uc_sfrcondo_tier_0.33_0.67_sm_sa_month.csv");
+    const { prices, isLoading } = useCSV(csvFilePath)
 
-    Papa.parse(csvFilePath, {
-        header: true,
-        download: true,
-        skipEmptyLines: true,
-        complete: function(results) {
-            console.log(results)
-        }
-    })
-
+    console.log("In History, prices:", prices)
+    console.log("In History, isLoading:", isLoading)
+    
     return (
-        <h1>Testing. 1, 2, 3...</h1>
+        <>
+            {/* { console.log(city) } */}
+            <h1>Testing. 1, 2, 3...</h1>
+        </>
+        
     )
 }
 
