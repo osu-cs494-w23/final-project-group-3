ab3507e1c8389bc0bd5f6450ab62cb2eaf30e063
Seth Weiss <sweisss@gmail.com>
2023-03-13 23:02:59 -0700

    Remove old code

--- src/components/History.js
+++ src/components/History.js
@@ -45,32 +45,22 @@ function History() {
     const [ searchParams, setSearchParams ] = useSearchParams()
     const [ inputQuery, setInputQuery ] = useState(searchParams.get("q") || "")
 
-    const [ text, setText ] = useState("")
     const { data, isLoading } = useCSV(csvFilePath)
     const regions = data.data
-
-    // const targetRegion = 'New York, NY'
-    const targetRegion = 'New York'
-    // const targetRegion = 'Chicago, IL'
     
     return (
         <>
             <form onSubmit={e => {
                 e.preventDefault()
-                // setInputQuery(text)
                 setSearchParams({ q: inputQuery })
                 console.log(searchParams.get("q"))
             }}>
-                {/* <input placeholder="Enter a region" onChange={(e) => setText(e.target.value)}/> */}
                 <input placeholder="Enter a region" value={inputQuery} onChange={e => setInputQuery(e.target.value)}/>
-                {/* <input placeholder="Enter a region" onChange={e => setInputQuery(e.target.value)}/> */}
-                {/* {console.log(text)} */}
                 <button type="submit">Search</button>
             </form>
             {isLoading && <Loading />}
             {regions && searchParams.get("q")
                 && regions.map(region => (
-                    // verifyInput(targetRegion, region.RegionName) && <Region key={region.RegionID} region={region} display={targetRegion} />
                     verifyInput(searchParams.get("q"), region.RegionName) && <Region key={region.RegionID} region={region} display={searchParams.get("q")} />
                     ))
             }
