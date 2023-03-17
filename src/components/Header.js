7ecffc48cb95deb05e014fa69ddd18d0eb2d59e8
Seth Weiss <sweisss@gmail.com>
2023-03-16 18:36:37 -0700

    Add styling to Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,21 @@ import styled from '@emotion/styled/macro'
 
 import Navbar from "./Navbar";
 
+const HeaderContainer = styled.div`
+    position: fixed;
+    top: 0;
+    display: flex;
+    flex-flow: column;
+    width: 100%;
+    overflow: hidden;
+    z-index: 1;
+`
+
 function Header() {
     return (
-        <>
+        <HeaderContainer>
             <Navbar />
-        </>
+        </HeaderContainer>
     )
 }
 
