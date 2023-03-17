fff91f88fe242bb73493af9cbcc256a52bb99055
Seth Weiss <sweisss@gmail.com>
2023-03-16 18:12:55 -0700

    Start Navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from '@emotion/styled/macro'
+
+function Navbar() {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Search Listings</li>
+            <li>Market History</li>
+            <li>Favorites</li>
+        </ul>
+    )
+}
+
+export default Navbar
