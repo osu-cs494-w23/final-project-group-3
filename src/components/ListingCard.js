9bedf73abd62d232cf7b96ae65f83bc1124a47d5
Seth Weiss <sweisss@gmail.com>
2023-03-16 21:17:16 -0700

    Update ListingCard style and placeholder values

--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,14 +1,15 @@
 import styled from '@emotion/styled/macro'
 
-const ListingContainer = styled.div`
-    border: 1px solid dimgray;
+const ListingContainer = styled.span`
+    border: 1px solid red;
     margin: 3px;
     padding: 5px;
     background-color: ghostwhite;
+    min-width: 190px;
 `
 
 const ImageContainer = styled.div`
-    border: 1px solid dimgray;
+    border: 1px solid blue;
     img{
         background-color: white;
         border: 1px solid dimgray;
@@ -16,8 +17,12 @@ const ImageContainer = styled.div`
         margin-left: auto;
         margin-right: auto;
         margin-bottom: 5px;
-        max-width: 100px;
+        max-width: 190px;
     }
+`
+
+const DataContainer = styled.div`
+    font-size: 12px;
 `
 
 function ListingCard(props) {
@@ -26,7 +31,13 @@ function ListingCard(props) {
             <ImageContainer>
                 <img src="https://i.pinimg.com/originals/2e/dc/4b/2edc4b5f7279d8d9bfbae04a75e104a8.jpg" alt="example image for listing" />
             </ImageContainer>
-            <p>Example desciption of listing</p>
+            <DataContainer>
+                <h2>$Listing Price</h2>
+                <p># bds | # ba | # sqft | (house) for (sale)</p>
+                <p>Address, City, State</p>
+                <p>Realtor</p>
+            </DataContainer>
+
         </ListingContainer>
     )
 
