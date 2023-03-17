af7b9dcc459b4d5dd297c98b792a610c99a91547
Seth Weiss <sweisss@gmail.com>
2023-03-16 18:28:27 -0700

    Add styling to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,40 @@
 import React from "react";
 import styled from '@emotion/styled/macro'
 
+const NavbarContainer = styled.div`
+    display: flex;
+    overflow: hidden;
+    background-color: dodgerblue;
+    z-index: 1;
+    top: 0;
+    padding: 0;
+    padding-bottom: 0px;
+    justify-content: space-evenly;
+    a{
+        color: black;
+        text-align: center;
+        padding: 14px 20px;
+        text-decoration: none;
+        font-size: 25px;
+    }
+    a:hover{
+        background-color: DeepSkyBlue;
+        color: Cornsilk;
+    }
+    a.active{
+        background-color: black;
+        color: #ddb122;
+    }
+`
+
 function Navbar() {
     return (
-        <ul>
-            <li>Home</li>
-            <li>Search Listings</li>
-            <li>Market History</li>
-            <li>Favorites</li>
-        </ul>
+        <NavbarContainer>
+            <a>Home</a>
+            <a>Search Listings</a>
+            <a>Market History</a>
+            <a>Favorites</a>
+        </NavbarContainer>
     )
 }
 
