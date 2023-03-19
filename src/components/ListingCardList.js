54a9c3fb9bdb2d7982810a4a01fa5da2135d118f
Seth Weiss <sweisss@gmail.com>
2023-03-18 18:12:29 -0700

    Import Redfin hooks to ListingCard

--- src/components/ListingCardList.js
+++ src/components/ListingCardList.js
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled/macro'
 
+import {useRedfinListingsApi, useRedfinAutoCompleteApi} from "../hooks/useRedfinApi";
 import ListingCard from './ListingCard'
 
 const ListingList = styled.ul`
@@ -11,6 +12,18 @@ const ListingList = styled.ul`
 
 function ListingCardList(props) {
     const region = props.region ? props.region : "your area"
+    const [ autoComplete ] = useRedfinAutoCompleteApi(props.region, false)
+    const id = autoComplete.data.exactMatch ? autoComplete.data.payload.exactMatch.id : null
+    console.log(autoComplete.data.payload)
+    const [listings] = useRedfinListingsApi({
+        region_id: '30749',
+        region_type: '6',
+        uipt: '1,2,3,4,7,8',
+        status: '9',
+        sf: '1,2,3,5,6,7',
+        num_homes: '10',
+      }, false);
+    console.log(listings)
 
     return (
         <>
