60664c4665835f18862781c97db3e8d8262ecd9d
Seth Weiss <sweisss@gmail.com>
2023-03-19 22:25:19 -0700

    Render favorites

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,14 +1,17 @@
 import React from "react"
 import styled from '@emotion/styled/macro'
 import { useOutletContext } from "react-router-dom";
+import ListingCard from "./ListingCard";
 
 function Favorites() {
 
     const { getFavoriteListings, addFavoriteListing, removeFavoriteListing } = useOutletContext()
+    const favoritesList = getFavoriteListings()
+    const listingIds = Object.keys(favoritesList)
 
     return (
         <>
-            {console.log(getFavoriteListings())}
+            {listingIds.map(id => <ListingCard key={id[0]} context={useOutletContext} id={id[0]} />)}
             <h1>This is the favorites route</h1>
         </>
     )
