f54d181d5781fd27cf7bd4c9f7c8c4441cda42ad
Luke Fernandez <luke.matthew.fernandez@gmail.com>
2023-03-20 12:59:04 -0700

    implement onSubmit

--- src/components/SearchListingsPage.js
+++ src/components/SearchListingsPage.js
@@ -1,4 +1,6 @@
 import styled from "@emotion/styled/macro";
+import { useState } from "react";
+import { useRedfinApiPropertyListingsFromLocation} from "../hooks/useRedfinApi";
 
 function SearchListingsPage(props) {
   const Container = styled.div({
@@ -7,9 +9,16 @@ function SearchListingsPage(props) {
     padding: "0px",
     margin: "0px",
   });
+
+
+  const onSearch = (location, searchFilters) => {
+    console.log(location);
+    console.log(searchFilters);
+  }
+
   return (
       <Container>
-        <SearchFilterBar></SearchFilterBar>
+        <SearchFilterBar onSearch={onSearch}></SearchFilterBar>
         <SearchResults></SearchResults>
       </Container>
   )
@@ -34,14 +43,28 @@ function SearchFilterBar(props) {
     margin: "5px",
   });
 
+  const onSearch = props.onSearch;
+  
   const onSubmit = (event) => {
     event.preventDefault();
+    const location = document.getElementById("location-form").value;
+    const minPrice = document.getElementById("min-price-form").value;
+    const maxPrice = document.getElementById("max-price-form").value;
+    const minBeds = document.getElementById("min-beds-form").value;
+    const minBaths = document.getElementById("min-baths-form").value;
+    const searchFilters = {
+      'min_price': Math.min(minPrice, maxPrice),
+      'max_price': Math.max(minPrice, maxPrice),
+      'min_beds': minBeds,
+      'min_baths': minBaths,
+    };
+    onSearch(location, searchFilters);
   }
 
   // A horizontal bar with a search box and a search button
   return (
     <Container>
-      <form onSubmit={(event) => onSubmit}>
+      <form onSubmit={(event) => onSubmit(event)}>
         <Input type="text" placeholder="City, ZIP, Address" required />
         <Input id={"min-price-form"} type="number" placeholder="Min Price" min={0}  />
         <Input id={"max-price-form"} type="number" placeholder="Max Price" min={0} />
