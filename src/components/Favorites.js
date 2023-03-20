5d15c55b761a17f7c368e4456e980165bbc7ec10
Seth Weiss <sweisss@gmail.com>
2023-03-20 09:20:11 -0700

    Add styled listing container

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -3,15 +3,24 @@ import styled from '@emotion/styled/macro'
 import { useOutletContext } from "react-router-dom";
 import ListingCard from "./ListingCard";
 
-function Favorites() {
+const ListingGrid = styled.div`
+    // border: 1px solid dimgray;
+    display: grid;
+    grid-template-columns: auto auto auto auto;
+    justify-content: center;
+    padding: 0;
+`
 
+function Favorites() {
     const { getFavoriteListings, addFavoriteListing, removeFavoriteListing } = useOutletContext()
     const favoritesList = getFavoriteListings()
     const listingIds = Object.keys(favoritesList)
 
     return (
         <>
-            {listingIds.map(id => <ListingCard key={id[0]} context={useOutletContext} id={id[0]} />)}
+            <ListingGrid>
+                {listingIds.map(id => <ListingCard key={id[0]} context={useOutletContext} id={id[0]} />)}
+            </ListingGrid>
             <h1>This is the favorites route</h1>
         </>
     )
