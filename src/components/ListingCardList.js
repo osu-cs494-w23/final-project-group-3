1ec041f1f4c63adf1e584250824224fd1cbec1aa
Seth Weiss <sweisss@gmail.com>
2023-03-20 14:38:01 -0700

    Import redfin hook

--- src/components/ListingCardList.js
+++ src/components/ListingCardList.js
@@ -2,6 +2,7 @@ import styled from '@emotion/styled/macro'
 import { useState } from 'react'
 import { useOutletContext } from "react-router-dom";
 
+import { useRedfinApiPropertyListingsFromLocation } from '../hooks/useRedfinApi'
 import ListingCard from './ListingCard'
 
 const ListingList = styled.ul`
@@ -15,7 +16,16 @@ function ListingCardList(props) {
     const region = props.region ? props.region : "your area"
     let id = 0
 
-    const listings = [
+    const [listings] = useRedfinApiPropertyListingsFromLocation(region,  null, false, 5)
+    // console.log(listings)
+    const listingsArray = listings.data
+    // console.log(listingsArray)
+    const listingsData = listingsArray.map(listing => listing.homes[0].homeData)
+    console.log("listingsData", listingsData)
+    {listingsData.map(listing => console.log("propertyId", listing.propertyId, "listing", listing))}
+    // if (listingsData[0].propertyId)
+
+    const placeholderListings = [
         <ListingCard key={id} context={useOutletContext} id={++id}/>,
         <ListingCard key={id} context={useOutletContext} id={++id}/>,
         <ListingCard key={id} context={useOutletContext} id={++id}/>,
@@ -27,7 +37,8 @@ function ListingCardList(props) {
         <>
             <h1>Featured listings in {region}</h1>
             <ListingList>
-                {listings}
+                {/* {placeholderListings} */}
+                {listingsData.map(listing => <ListingCard key={listing.propertyId} id={listing.propertyId} data={listing} context={useOutletContext}/>)}
             </ListingList>
         </>
     )
