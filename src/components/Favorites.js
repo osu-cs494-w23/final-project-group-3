4e9db449dcf418083ef86994749fcc6e0773e7ef
Seth Weiss <sweisss@gmail.com>
2023-03-19 21:35:09 -0700

    Import favorites list via useOutletContext

--- src/components/Favorites.js
+++ src/components/Favorites.js
@@ -1,9 +1,14 @@
 import React from "react"
 import styled from '@emotion/styled/macro'
+import { useOutletContext } from "react-router-dom";
 
 function Favorites() {
+
+    const { getFavoriteListings, addFavoriteListing, removeFavoriteListing } = useOutletContext()
+
     return (
         <>
+            {console.log(getFavoriteListings())}
             <h1>This is the favorites route</h1>
         </>
     )
